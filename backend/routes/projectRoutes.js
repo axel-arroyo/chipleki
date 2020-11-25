00fb6f2a8cf4c61080b60b2e8bd9eefb1c077181
Yi-Zhou-Zhou <65474467+Yi-Zhou-Zhou@users.noreply.github.com>
2020-11-25 03:03:09 -0300

    Add files via upload

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -12,28 +12,6 @@ router.get("/", async (req,resp) => {
     }
 });
 
-//Revisar proyecto
-router.get("/:id", async (req,resp) => {
-    try {
-        const allProject = await Project.findAll({where:req.params});
-        console.log(req.params);
-        resp.send(allProject);
-    } catch (error) {
-        resp.status(400).send("Error al hacer una query a la base de datos");
-    }
-});
-
-//Revisar requerimiento
-router.get("/:id/:id_req", async (req,resp) => {
-    try {
-        const requirement = await Project.findOne({where:req.params})
-        console.log(req.params);
-        resp.send(requirement)
-    } catch (error) {
-        resp.status(400).send("Error al hacer una query a la base de datos");
-    }
-});
-
 //Crear proyecto
 router.post("/", async (req,resp) => {
     try {
@@ -44,10 +22,21 @@ router.post("/", async (req,resp) => {
     }
 });
 
+//Todos los Requerimientos
+router.get("/requirement/", async (req,resp) => {
+    try {
+        const requirements = await Requirement.findAll();
+        resp.send(requirements);
+    } catch (error) {
+        resp.status(400).send("Error al hacer una query a la base de datos");
+    }
+});
+
 //Crear requerimientos al proyecto de id especificada
-router.post("/:id/", async (req,resp) => {
+router.post("/requirement/", async (req,resp) => {
     try {
         const requirement = await Requirement.create(req.body);
+        return resp.send(requirement);
     } catch (error) {
         resp.status(400).send(error);
     }
