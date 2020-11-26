1a20588a346bf1ac923d3b4e6db55231f1942ccd
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2020-11-25 23:44:23 -0300

    Refreshed fixed

--- frontend/src/Projects.js
+++ frontend/src/Projects.js
@@ -7,13 +7,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Col, Row, Container, Alert, Image } from "react-bootstrap";
 import { fetchProjects } from './redux/actions/projectActions';
 import {Link} from 'react-router-dom';
-import { fetchRequirements } from './redux/actions/requirementActions';
 
 
 function Projects(props){
 
     const isLogged = useSelector((store) => store.authReducer.isLogged);
-    const projects = useSelector((store) => store.projectReducer.projects);
+	const projects = useSelector((store) => store.projectReducer.projects);
 	const dispatch = useDispatch();
 	
     useEffect(() => {
@@ -30,20 +29,6 @@ function Projects(props){
 		}
 	}, [dispatch, isLogged]);
 
-	useEffect(() => {
-		if (isLogged) {
-			axios.get("http://localhost:8080/project/requirement", {
-				})
-				.then((data) => {
-					console.log(data);
-					dispatch(fetchRequirements(data.data));
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	}, [dispatch, isLogged]);
-
     return isLogged ? (
         <Container fluid>
             <Link to="/newProject">
@@ -53,11 +38,16 @@ function Projects(props){
                 <Image src={plus} className="newIcon"/>
             </Link> 
 			<Row>
-				{projects.map((v) => (
+				{
+				projects !== undefined ?
+				projects.map((v) => (
 					<Col key={v.id} md={2}>
-						<Project id={v.id} flag="true" />
+						<Project id={v.id}  flag="true" />
 					</Col>
-				))}
+				)):
+				<Col>
+				</Col>
+				}
 			</Row>
 		</Container>
 		
